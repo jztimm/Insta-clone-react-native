e4f507f015685259a26c584e1c5b2a656c9b5ee5
jzavier <jztimm@gmail.com>
2022-02-25 12:11:44 -0500

    doen with header comp

--- components/home/Header.js
+++ components/home/Header.js
@@ -10,6 +10,39 @@ const Header = () => {
 					source={require('../../assets/Instagram-logo-white.png')}
 				/>
 			</TouchableOpacity>
+
+			<View style={styles.iconsContainer}>
+				<TouchableOpacity>
+					<Image
+						source={{
+							uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png',
+
+						}}
+						style={styles.icon}
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity>
+					<Image
+						source={{
+							uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png',
+
+						}}
+						style={styles.icon}
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity>
+					<View style={styles.unreadBadge}>
+						<Text style={styles.unreadBadgeText}>11</Text>
+					</View>
+					<Image
+						source={{
+							uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png',
+
+						}}
+						style={styles.icon}
+					/>
+				</TouchableOpacity>
+			</View>
 		</View>
 	)
 }
@@ -25,7 +58,33 @@ const styles = StyleSheet.create({
 		width: 100,
 		height: 50,
 		resizeMode: 'contain'
-	}
+	},
+	iconsContainer: {
+		flexDirection: 'row',
+	},
+	icon: {
+		width: 30,
+		height: 30,
+		marginLeft: 10,
+		resizeMode: 'contain'
+	},
+	unreadBadge: {
+		backgroundColor: '#FF3250',
+		position: 'absolute',
+		left: 25,
+		bottom: 18,
+		width: 25,
+		height: 18,
+		borderRadius: 25,
+		alignItems: 'center',
+		justifyContent: 'center',
+		zIndex: 100
+	},
+	unreadBadgeText: {
+		color: 'white',
+		fontWeight: '600',
+		paddingBottom: 1
+	},
 })
 
 export default Header
